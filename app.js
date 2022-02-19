c9b5cff3ef74434074dcd5a1fe35112648c6c8dd
Abdullah UĞUZ <abdullahuguz08@gmail.com>
2022-02-20 00:53:48 +0300

    blog added

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'photo name',
-    desc: 'photo info',
-  };
+    const blog = { 
+        id: 1, 
+        title: "Blog title", 
+        description: "Blog description" 
+    }
   res.send(photo);
 });
 
