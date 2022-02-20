8ae49890a898bb9416654a7d0831d989667d174e
Abdullah UĞUZ <abdullahuguz08@gmail.com>
2022-02-20 22:13:43 +0300

    ejs added

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express');
+const path=require('path');
+const ejs = require('ejs');
 
 const app = express();
 
+// ejs
+app.set("view engine","ejs");
+
+// middleware
+app.use(express.static('public'));
+
+// router
 app.get('/', (req, res) => {
-    const blog = { 
-        id: 1, 
-        title: "Blog title", 
-        description: "Blog description" 
-    }
-  res.send(photo);
+
+  res.render('index');
 });
+
+app.get('/about', (req, res) => {
+
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+
+  res.render('post');
+});
+
+
 
 const PORT = 4000;
 
