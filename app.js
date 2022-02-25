498baa95ea0ed453d6804323d93866c219858eed
Abdullah UĞUZ <abdullahuguz08@gmail.com>
2022-02-26 00:58:13 +0300

    put delete operation added and mvc  added

--- app.js
+++ app.js
@@ -4,64 +4,51 @@ const mongoose = require('mongoose');
 
 const ejs = require('ejs');
 
-const path=require('path');
+const path = require('path');
 
-const Post=require('./models/Post')
+const methodOverride = require('method-override');
+
+const postController = require('./controller/postController');
+
+const pageController = require('./controller/pageController');
 
 const app = express();
 
 // ejs
-app.set("view engine","ejs");
-
+app.set('view engine', 'ejs');
 
 // connection db
 mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
 // middleware
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
-// router
-app.get('/', async (req, res) => {
-  const posts= await Post.find({});
-  res.render('index',{
-    posts
-  });
-});
+// GET
+app.get('/', postController.getAllPosts);
 
-app.get('/posts/:id', async (req, res) => {
+app.get('/posts/:id', postController.getPost);
 
-  const post=await Post.findById(req.params.id);
-  res.render('post',{
-    post
-  });
-});
+app.get('/posts/edit/:id', pageController.getEditPage);
 
-app.get('/about', (req, res) => {
+app.get('/about', pageController.getAboutPage);
 
-  res.render('about');
-});
+app.get('/add_post', pageController.getAdd_PostPage);
 
-app.get('/add_post', (req, res) => {
+// POST
+app.post('/posts', postController.createPost);
 
-  res.render('add_post');
-});
+// PUT
+app.put('/posts/:id', postController.updatePost);
 
-
-/*
-app.get('/post', (req, res) => {
-
-  res.render('post');
-});
-*/
-
-// post
-app.post('/posts', async(req,res) =>{
-  await Post.create(req.body);
-//  console.log(req.body);
-  res.redirect('/');
-})
+// DELETE
+app.delete('/posts/:id', postController.deletePost);
 
 const PORT = 4000;
 
